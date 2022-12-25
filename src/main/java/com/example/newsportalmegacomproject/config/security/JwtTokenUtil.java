afd0a3f01172a6bb5c08a0c182c700dde446e325
Datka <datkascnu@gmail.com>
2022-12-26 00:05:11 +0600

    added dependency

--- src/main/java/com/example/newsportalmegacomproject/config/security/JwtTokenUtil.java
+++ src/main/java/com/example/newsportalmegacomproject/config/security/JwtTokenUtil.java
@@ -1,5 +1,9 @@
 package com.example.newsportalmegacomproject.config.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
