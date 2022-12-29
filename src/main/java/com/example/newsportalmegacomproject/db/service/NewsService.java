78e01a66da0c212cfef4894637bb655045ce50e2
Datka <datkascnu@gmail.com>
2022-12-30 00:15:43 +0600

    getAllNews method

--- src/main/java/com/example/newsportalmegacomproject/db/service/NewsService.java
+++ src/main/java/com/example/newsportalmegacomproject/db/service/NewsService.java
@@ -156,9 +156,15 @@ public class NewsService {
         User user = authenticateUser();
         List<News> news = newsRepository.getAllNewsSortedByIds();
         List<NewsResponse> newsResponses = new ArrayList<>();
+        List<News> allNews = new ArrayList<>();
+        List<Favorite> userFavorites = user.getFavorites();
+        for (Favorite fav: userFavorites) {
+            allNews.add(fav.getNews());
+        }
+
         for (News n : news) {
             NewsResponse newsResponse = new NewsResponse(n);
-            if (user.getFavorites() != null) {
+            if (allNews.contains(n)) {
                 for (Favorite fav : user.getFavorites()) {
                     if (fav.getNews().equals(n)) {
                         newsResponse.setIsFavorite(true);
